2f8b3fdca3a61ea4fd794f1881644ec41ad86e7d
Nishant Sagar <nishant@MacBookAir.lan>
2025-06-30 23:33:15 +0530

    Add homepage content

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, index
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('index/', index, name='index'),
+    # path('', home, name='home'),
+    path('', index, name='index'),
 ]
